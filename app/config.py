4aafbc84fa936b014808f50b270d99ca2dd7740b
Miguel González López UDC <58361045+mgonzalezlopezudc@users.noreply.github.com>
2026-03-06 21:44:42 +0100

    chore: use context-provider host for Orion registrations

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
     ORION_BASE_URL = os.getenv("ORION_BASE_URL", "http://localhost:1026")
     ORION_TIMEOUT = float(os.getenv("ORION_TIMEOUT", "8"))
-    CONTEXT_PROVIDER_URL = os.getenv("CONTEXT_PROVIDER_URL", "http://tutorial:3000")
+    CONTEXT_PROVIDER_URL = os.getenv("CONTEXT_PROVIDER_URL", "http://context-provider:3000")
     APP_BASE_URL = os.getenv("APP_BASE_URL", "http://host.docker.internal:5000")
     FIWARE_SERVICE = os.getenv("FIWARE_SERVICE", "")
     FIWARE_SERVICEPATH = os.getenv("FIWARE_SERVICEPATH", "/")
